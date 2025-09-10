388d57796f3fb1263d36d46ad59d47f86b104b96
--global <lucasbruck04@gmail.com>
2025-09-10 18:12:59 -0300

    Condigo basicamente finalizado com a adiçao do main e detalhamento do readme, alem da atualizaçÃo do requirementes.txt

--- README.md
+++ README.md
@@ -1,3 +1,60 @@
-# Australia visa checker
+# Australia Visa Checker
 
-Este repositorio tem como objetivo entrar no site o oficial do governo da Australia e checar se foram abertas vagas para visa de trabalho em feriados. Alem de enviar uma mensagem/email para a pessoa avisando que abriu essa vaga
+This project aims to automatically monitor the status of Working Holiday Visa openings for Brazil on the official Australian government website.  
+When the status changes (for example, when new spots open), the script sends an alert email to the registered recipients.
+
+---
+
+## Features
+
+- Accesses the official Australian government website and checks the visa status for Brazil.
+- Checks the last update date of the status.
+- Automatically sends an email to the registered recipients when the status changes to open.
+
+---
+
+## How to Use
+
+### 1. Prerequisites
+
+- Python 3 installed
+- Install the project dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+### 2. Configure `.env` file
+
+Create a `.env` file in the project root whith the following content
+
+```file
+EMAIL_SENDER=youremail@gmail.com
+PSW_EMAIL=your_app_password
+LIST_EMAIL_RECIVER=email1@gmail.com,email2@gmail.com
+```
+
+**Note:**
+For Gmail accounts, you must generate an app password and use it in the ```PSW_EMAIL``` field.
+
+### 3. Run the Script
+
+In the terminal, execute:
+
+```bash
+python main.py
+```
+
+If there is a change in the visa status, the registered recipients will receive an alert email.
+
+
+## Customization
+
+- To add more recipients, simply separate the emails with a comma in the `LIST_EMAIL_RECIVER` field in the `.env` file.
+- The script can be adapted to monitor other countries or visa types by adjusting the search logic in the code.
+
+---
+
+## Notes
+
+- The script was developed for educational purposes and can be adapted as needed.
+- Make sure your email is authorized to send messages via SMTP (Gmail requires an app password).
---
+++ main.py
@@ -0,0 +1,70 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
+
+# SESSAO PARA A CHECAGEM DE MUDANÇA DE STATUS E LAST UPDATE DO SITE
+url = "https://immi.homeaffairs.gov.au/what-we-do/whm-program/status-of-country-caps?utm_source=chatgpt.com"
+
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+tabela = soup.find("table")
+status_brasil = None
+
+if tabela:
+    linhas = tabela.find_all("tr")
+    for linha in linhas:
+        colunas = linha.find_all("td")
+        if colunas and "Chile" in colunas[0].text:
+            status_brasil = [coluna.text.strip() for coluna in colunas][1]
+            status_brasil = status_brasil.replace('\u200b', '').strip()
+            break
+
+text_last_upd = None
+for div in soup.find_all("div"):
+    if "Last updated" in div.text:
+        text_last_upd = div.text.strip() 
+        break
+match = re.search(r"(\d{1,2}/\d{1,2}/\d{4})", text_last_upd)
+if match:
+    date_str = match.group(0)
+    last_upd_data = datetime.strptime(date_str, "%d/%m/%Y")
+    # print(last_upd_data)
+
+# SESSAO PARA O ENVIO DE EMAIL CASO HAJA A MUDANÇA DE STATUS
+import smtplib
+from email.mime.text import MIMEText
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+EMAIL_SENDER = os.getenv("EMAIL_SENDER")
+EMAIL_PSW = os.getenv("PSW_EMAIL")
+
+def send_email(subject, body, to_email):
+    from_email = EMAIL_SENDER
+    psw = EMAIL_PSW
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+    msg["From"] = from_email
+    msg["To"] = to_email
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+        server.login(from_email, psw)
+        server.sendmail(from_email, [to_email], msg.as_string())
+list_status = ["paused", "closed"]
+
+if any(s in status_brasil for s in list_status):
+    print("Status it is 'paused' or 'closed'")
+else:
+    for email in os.getenv("LIST_EMAIL_RECIVER").split(","):
+        print(email)
+        print("EMAIL SENT")
+        send_email(
+            "Change in Australia Visa!!",
+            f'New status: {status_brasil} \nLast update: {last_upd_data}\n'
+            'Go to: https://online.immi.gov.au/ola/app and secure your spot',
+            email
+        )
